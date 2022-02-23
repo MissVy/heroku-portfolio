7fbddcdbf5d95f1e4b2e5f82c7f53aa6005ad2ff
Robbie McGregor <mr.robbie.mcgregor@gmail.com>
2022-02-23 20:34:25 +1300

    initial test commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const express = require("express")
+const app = express()
+const path = require('path');
+process.env.PWD = process.cwd()
+app.use(express.static(process.env.PWD + '/public'));
+// app.use(express.static(__dirname + '/public'));
+let port = process.env.PORT || 80
+
+
+
+
+app.get('/', (req, res) => {
+    res.redirect(`/index.html`)
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname + `/${req.url}`))
+})
+
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`)
+})
